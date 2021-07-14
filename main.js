8a9ad0b0a39b29c6f4cb276b028613475e7d0c0b
Advath Coder <72031535+Advaith6@users.noreply.github.com>
2021-07-14 15:36:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+var text_1 = "Advaith";
+var text_2 = "  Prakash";
+var text_3 = text_1.concat(text_2);
+console.log(text_3);
+
+var number_1 = 53;
+var number_2 = 99;
+var add = number_1 + number_2;
+console.log(add);
+
+var sub = number_2 - number_1;
+console.log(sub);
+
+var multiply = number_1 * number_2;
+console.log(multiply);
+
+var divide = number_2 + number_1;
+console.log(divide);
